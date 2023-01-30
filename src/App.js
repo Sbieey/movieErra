7a0d68bc1206cbe95dacd596f8299e35f5bf2e92
sbieey <brainbox.sb@gmail.com>
2023-01-30 01:09:43 +0100

    movies search application use live API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import { useEffect } from 'react'
 import './App.css';
+import MovieCard from './MovieCard';
+import SearchIcon from "./search.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//43f2284b
+
+const API_URL = 'http://www.omdbapi.com?apikey=43f2284b';
+
+// const movie1 = 
+//   {
+//     "Title": "Man of Steel",
+//     "Year": "2013",
+//     "imdbID": "tt0770828",
+//     "Type": "movie",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BMTk5ODk1NDkxMF5BMl5BanBnXkFtZTcwNTA5OTY0OQ@@._V1_SX300.jpg"
+// }
+
+
+const App = () => {
+
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  }
+  // const searchLatest = async (year) => {
+  //   const response = await fetch(`${API_URL}&s={}`)
+  // }
+  useEffect(() => {
+    searchMovies('');
+  }, []); 
+
+  return(
+    <div className='app'>
+      <h1>MovieErra</h1>
+
+      <div className='search'>
+        <input 
+        placeholder="Search for movies"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        />
+
+        <img 
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0
+        ? (
+           <div className='container'>
+            { movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))};
+           </div>
+          )
+          : (
+            <div className='empty'>
+              <h2>No movies found</h2>
+            </div>
+          )}
+     
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const App = () => {
+//   return (
+//     <div>
+//         <h1>How to code</h1>
+//     </div>
+//   )
+// }
+// const jobs = [
+//   {id: 1, isActive: true},
+//   {id: 2, isActive: true},
+//   {id: 3, isActive: false}
+// ]
+
+// const activeJobs = jobs.filter((job)=>{return job.isActive})
+// console.log(activeJobs)
